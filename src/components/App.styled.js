fc6f8c6aa101de66acc00c5f9063d0a0d77dff40
Dmytro Chushko <dmytro.chushko@gmail.com>
2022-07-31 18:07:17 +0300

    fix App.styled.js

--- src/components/App.styled.js
+++ src/components/App.styled.js
@@ -1,12 +1,12 @@
 import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
 
-export const Container = styled(div)`
+export const Container = styled.div`
   margin-left: auto;
   margin-right: auto;
   max-width: 1200px;
 `;
-export const Navigation = styled(ul)`
+export const Navigation = styled.ul`
   display: flex;
   gap: 10px;
 `;
